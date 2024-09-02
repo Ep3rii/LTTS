1633dabea250f9c2ed88a8d944107b2dd97124ac
Euzébio Pereira da Silva Filho <euzzbs@gmail.com>
2024-09-01 23:49:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,51 +2,122 @@ import cv2
 import pytesseract
 from gtts import gTTS
 import os
-import numpy as np
 import streamlit as st
+import numpy as np
 from PIL import Image
+
+# Configuração do caminho do Tesseract no Windows
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # Função para processar a imagem e aplicar OCR
 def process_frame(image):
-    # Converter para tons de cinza
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # Ruído
-    gray = cv2.medianBlur(gray, 3)
-
-    # Aplicar binarização para converter em preto e branco
-    _, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-
-    # Configurações do Tesseract
+    kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+    sharp = cv2.filter2D(gray, -1, kernel)
+    bright = cv2.convertScaleAbs(sharp, alpha=1.2, beta=30)
+    _, binary = cv2.threshold(bright, 127, 255, cv2.THRESH_BINARY)
     config = '--oem 3 --psm 11'
-
-    # Aplicar OCR no frame binário
     text = pytesseract.image_to_string(binary, config=config)
     
-    return text
+    return text, binary
 
-# Interface do Streamlit
-st.title("OCR com Tesseract e Streamlit")
-st.write("Carregue uma imagem para realizar OCR e ouvir o texto reconhecido.")
+# Função para identificar o tipo de texto
+def identify_text_type(text):
+    words = text.split()
+    if len(words) == 1:
+        return "Palavra Única"
+    elif len(words) > 1 and len(words) <= 10:
+        return "Frase"
+    else:
+        return "Parágrafo"
 
-# Opção para carregar imagem
-uploaded_file = st.file_uploader("Escolha uma imagem...", type=["jpg", "jpeg", "png"])
+# Configurações de estilo e layout do Streamlit
+st.set_page_config(page_title="LTTR", page_icon="📖", layout="wide")
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    image = np.array(image)
+# Exibir cabeçalho fixo no canto superior esquerdo
+st.header("LTTR")
+st.markdown("Leitura de Texto em Tempo Real")
 
-    text = process_frame(image)
+# Menu lateral com opções de captura de imagem ou upload
+st.sidebar.title("Menu")
+option = st.sidebar.radio("Escolha uma opção:", ("Captura de Imagem", "Carregar Imagem"))
+
+# CSS para centralizar a imagem e definir tamanho original
+st.markdown("""
+<style>
+    .stImage {
+        display: flex;
+        justify-content: center;
+    }
+    .stImage > img {
+        max-width: none;
+        width: auto;
+        height: auto;
+        margin: auto;
+    }
+</style>
+""", unsafe_allow_html=True)
+
+# Interface principal
+if option == "Captura de Imagem":
+    st.title("Captura de Imagem")
+    cap = cv2.VideoCapture(0)
+
+    if st.button("Capturar"):
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
+        text, processed_image = process_frame(frame)
+        
+        if text:
+            st.write("Texto reconhecido:")
+            st.text(text)
+            
+            text_type = identify_text_type(text)
+            st.write(f"Tipo de Texto: {text_type}")
+            
+            tts = gTTS(text, lang='pt')
+            tts.save("output.mp3")
+            st.audio("output.mp3", format="audio/mp3")
+        
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.write("Imagem Capturada Original:")
+            st.image(frame, channels="BGR", use_column_width=True, clamp=True)
+        
+        with col2:
+            st.write("Imagem Pós-Processada (Preto e Branco):")
+            st.image(processed_image, channels="GRAY", use_column_width=True, clamp=True)
+
+    cap.release()
+
+elif option == "Carregar Imagem":
+    st.title("Upload de Imagem")
+    uploaded_file = st.file_uploader("Escolha uma imagem...", type=["jpg", "jpeg", "png"])
     
-    if text:
-        st.write("Texto reconhecido:")
-        st.text(text)
-        tts = gTTS(text, lang='pt')
-        tts.save("output.mp3")
-        st.audio("output.mp3", format="audio/mp3")
-    
-    # Exibir a imagem carregada
-    st.image(image, channels="RGB")
-
-# Nota para usuários
-st.write("A funcionalidade de captura de vídeo em tempo real não está disponível no Streamlit online.")
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        image = np.array(image)
+        
+        text, processed_image = process_frame(image)
+        
+        if text:
+            st.write("Texto reconhecido:")
+            st.text(text)
+            
+            text_type = identify_text_type(text)
+            st.write(f"Tipo de Texto: {text_type}")
+            
+            tts = gTTS(text, lang='pt')
+            tts.save("output.mp3")
+            st.audio("output.mp3", format="audio/mp3")
+        
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.write("Imagem Carregada Original:")
+            st.image(image, channels="RGB", use_column_width=True, clamp=True)
+        
+        with col2:
+            st.write("Imagem Pós-Processada (Preto e Branco):")
+            st.image(processed_image, channels="GRAY", use_column_width=True, clamp=True)
